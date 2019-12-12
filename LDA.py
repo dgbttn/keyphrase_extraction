937009647d9a2f1edd0f4b6f9e08b2833fbd5873
dgbttn <dgbttn@gmail.com>
2019-12-13 00:46:23 +0700

    add get_candidate

--- LDA.py
+++ LDA.py
@@ -1,5 +1,20 @@
-from os import listdir
-from os.path import isfile, join
+import gensim
+import gensim.corpora as corpora
+import pyLDAvis
+import pyLDAvis.gensim 
 
+from reader import *
+from preprocessing import *
 
-file_path = 'data_original_files0-9999'
+if __name__ == '__main__':
+    folder_path = 'datasets/data_original_files0-9999'
+    file_list = get_file_list(folder_path)
+
+    data = []
+    for a, c in file_list:
+        about_text = read_text_file(a)
+        content_text = read_text_file(c)
+        doc = get_candidates(about_text) + get_candidates(content_text)
+        data.append(doc)
+
+    
--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,10 @@
 import string
 import re
+from vncorenlp import VnCoreNLP
 
 from reader import FileType
 
+annotator = VnCoreNLP("VnCoreNLP-1.1.1.jar", annotators="wseg,pos,ner,parse", max_heap_size='-Xmx2g')
 
 def remove_punctuations(text):
     puncs = set(re.sub(r"[/.-]", "", string.punctuation))
@@ -16,9 +18,38 @@ def get_main_text(text):
     return max(text.split('|'), key=len)
 
 
+def lemmatize(tags, allowed_postags=['A', 'N', 'Ny', 'Np', 'V', 'Z']):
+    return [word for (word, tag) in tags if tag in allowed_postags]
+
+
+def pos_tagging(text):
+    annotated_text = annotator.annotate(text)
+    sentences = annotated_text['sentences']
+    text_out = []
+    for sen in sentences:
+        new_sen = [(w['form'], w['posTag']) for w in sen]
+        text_out = text_out + new_sen
+    return text_out
+
+
 def preprocessing(text, file_type=None):
+    text = text.strip()
     if file_type == FileType.CONTENT:
         text = get_main_text(text)
 
+    if file_type == FileType.ABOUT:
+        vv = ['V/v', 'v/v', 'Về', 'về', 'Về việc', 'về việc']
+        for v in vv:
+            if text.find(v)>-1:
+                text = text.replace(v, '', 1)
+                break
+        
     text = remove_punctuations(text)
+    return text
+
+
+def get_candidates(text):
+    text = preprocessing(text)
+    text = lemmatize(pos_tagging(text))
+    # text = list(set(text))
     return text
